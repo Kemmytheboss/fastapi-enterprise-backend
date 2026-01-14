b9bece7a50570fb7e60a8d940071587b670db6f4
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-14 14:54:12 +0300

    added login router to validate if the user exists, if not, generates error

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -9,3 +9,15 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/register", response_model=UserResponse)
 def register(user_create: UserCreate, db: Session = Depends(get_db)):
     return register_user(db, email=user_create.email, password=user_create.password)
+
+@router.post("/login")
+def login(user_login: UserLogin, db: Session = Depends(get_db)):
+    user = authenticate_user(db, email=user_login.email, password=user_login.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = login_user(user)
+    return {"access_token": access_token, "token_type": "bearer"}
